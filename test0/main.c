4d00664962bfccdcd8abd45c2569c2000e1ab009
Felipe Operti <operti.felipe@gmail.com>
2025-10-27 23:40:03 +0100

    first commit

---
+++ test0/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a;
+    printf("Please enter a number: ");
+    scanf("%d", &a);
+    printf("The number choosen is %d",a );
+    return 0;
+}
